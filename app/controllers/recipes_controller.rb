d4f4f6e24799b366eb5926764057607585194fa8
Daniel Suarez <suarez.daniel101@gmail.com>
2023-02-01 22:55:04 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,34 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(:name)
+  end
 end
+
+#   def edit
+#     @ingredient = Ingredient.find(params[:id])
+#   end
+
+#   def update
+#     @ingredient = Ingredient.find(params[:id])
+#     @ingredient.update(ingredient_params)
+#     redirect_to @ingredient
+#   end
+
